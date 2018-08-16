9ca23302d375dca7bfdcbbc2f223476fd5f41c1b
Isaac Avram <avrisaac555@gmail.com>
2018-08-15 22:21:32 -0700

    Added assignment operators

--- src/common/ByteString.cpp
+++ src/common/ByteString.cpp
@@ -17,4 +17,42 @@ namespace sockets {
         _size = 0;
     }
 
+    ByteString& ByteString::operator=(const ByteString& other)
+    {
+        if(this != &other) 
+        {
+            // Reallocate memory if needed
+            if (_size != other._size) 
+            {
+                delete [] _data;
+                _data = new byte[other.size];
+                _size = other.size;
+            }
+            // Copy the data
+            std::copy(other.cbegin(), other.cend(), _data);
+        }
+        return *this;
+    }
+
+    ByteString& ByteString::operator=(ByteString&& other)
+    {
+        if(this != &other) 
+        {
+            // Free data if needed
+            if (_size > 0) 
+            {
+                delete [] _data;
+                _data = nullptr;
+                _size = 0;
+            }
+
+            // Copy objects here
+            _data = other._data;
+            _size = other._size;
+
+            // Free other's resources
+            delete [] other._data;
+            other._size = 0;
+        }
+    }
 }
