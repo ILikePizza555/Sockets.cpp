ff52bdbbce5c5cb7436588bf87607652a9f922ee
Isaac Avram <avrisaac555@gmail.com>
2018-08-15 21:49:26 -0700

    Added constructors and destructors

--- src/common/ByteString.cpp
+++ src/common/ByteString.cpp
@@ -1,5 +1,22 @@
 #include <include/ByteString.h>
+#include <algorithm>
 
+/**
+ * Allocates new space for a copy of the data, copies it, and then returns it as a const
+ */
+const sockets::byte* allocate_and_copy(const sockets::byte* data, size_t size)
+{
+    sockets::byte* rv = new sockets::byte[size];
+    std::copy(data, data + size, rv);
+    return static_cast<const sockets::byte*>(rv);
+}
 namespace sockets {
     ByteString::ByteString() : _data(nullptr), _size(0) {}
+    ByteString::ByteString(byte* data, size_t size) : _data(data), _size(size) {}
+    ByteString::ByteString(ByteString& other) : _data(allocate_and_copy(other._data, other._size)), _size(other.size) {}
+    ByteString::~ByteString() 
+    {
+        delete [] _data;
+        _data = nullptr;
+    }
 }
