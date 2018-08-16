88d694c3982f9b32ce41c1231604adb1709d619f
Isaac Avram <avrisaac555@gmail.com>
2018-08-15 21:55:04 -0700

    Fixed implementation for new interface

--- src/common/ByteString.cpp
+++ src/common/ByteString.cpp
@@ -1,22 +1,20 @@
 #include <include/ByteString.h>
 #include <algorithm>
 
-/**
- * Allocates new space for a copy of the data, copies it, and then returns it as a const
- */
-const sockets::byte* allocate_and_copy(const sockets::byte* data, size_t size)
-{
-    sockets::byte* rv = new sockets::byte[size];
-    std::copy(data, data + size, rv);
-    return static_cast<const sockets::byte*>(rv);
-}
+
 namespace sockets {
     ByteString::ByteString() : _data(nullptr), _size(0) {}
     ByteString::ByteString(byte* data, size_t size) : _data(data), _size(size) {}
-    ByteString::ByteString(ByteString& other) : _data(allocate_and_copy(other._data, other._size)), _size(other.size) {}
+    ByteString::ByteString(ByteString& other) : _data(new byte[other.size]), _size(other.size)
+    {
+        std::copy(other.cbegin(), other.cend(), _data);
+    }
+    ByteString::ByteString(ByteString&& other) : _data(std::move(other._data)), _size(std::move(other._size)) {}
     ByteString::~ByteString() 
     {
         delete [] _data;
         _data = nullptr;
+        _size = 0;
     }
+
 }
