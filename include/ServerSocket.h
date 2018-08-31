64903b20336e8262d86a8462b08d839dc5416833
Isaac Avram <avrisaac555@gmail.com>
2018-08-31 14:40:36 -0700

    Added declaration of ServerSocket

---
+++ include/ServerSocket.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include "Socket.h"
+
+namespace sockets {
+    class ServerSocket 
+    {
+        using std::string;
+
+        private:
+            std::string host;
+            std::string port;
+
+            sock_t _socket;
+        public:
+            ServerSocket(string host, string port);
+            ~ServerSocket();
+
+            /**
+             * Enables the socket to begin listening for connections
+             */
+            void listen(int backlog) const; 
+            
+            /**
+             * Establishes a connection with the first available client. If none exist, this function blocks until a connection is available.
+             */
+            Connection accept(size_t bufferCapacity);
+    };
+}
