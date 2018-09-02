8b3cb0e9b993de0744431ce2b3a470c1fcd2f4c9
Isaac Avram <avrisaac555@gmail.com>
2018-09-02 14:12:10 -0700

    Added implementation file for SocketException and updated CMake

---
+++ src/common/SocketException.cpp
@@ -0,0 +1,6 @@
+#include <SocketException.h>
+
+namespace sockets {
+    _Exception::_Exception(std::string function_name);
+    _Exception::_Exception(std::string class_name, std::string function_name);
+}
