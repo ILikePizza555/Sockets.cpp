edb425cc10889d7ed3ea5f5b6d74e2a548e71f3f
Isaac Avram <avrisaac555@gmail.com>
2018-09-02 16:33:04 -0700

    Updated documentation

--- include/SocketException.h
+++ include/SocketException.h
@@ -2,16 +2,22 @@
 
 #include <exception>
 #include <string>
+#include <sstream>
 #include <optional>
 #include "Socket.h"
 
 namespace sockets {
-
+    
+    /**
+     * Gets the last error code set by the system
+     */
     int get_error_code();
-    std::string get_error_message();
+    std::string get_error_message(int code);
 
     class _Exception : public std::exception
     {
+    protected:
+        std::stringstream ss;
     public:
         /** Throwing class name */
         const std::optional<std::string> class_name;
@@ -40,6 +46,18 @@ namespace sockets {
     public:
         ClosedError(std::string class_name, std::string function);
         
+        virtual const char* what() const noexcept;
+    }
+
+    class SocketError : public _Exception
+    {
+    public: 
+        const int error_code;
+        const std::string message;
+
+        SocketError(std::string class_name, std::string function_name, std::string message, int error_code);
+        SocketError(std::string function_name, std::string message, int error_code);
+
         virtual const char* what() const noexcept;
     }
 }
