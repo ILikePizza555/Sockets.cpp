339c3e260a56799922224508b502cac40f020762
Isaac Avram <avrisaac555@gmail.com>
2018-09-11 16:44:12 -0500

    Templated Connection.h

--- include/Connection.h
+++ include/Connection.h
@@ -4,29 +4,62 @@
 #include "ByteBuffer.h"
 #include "ByteString.h"
 #include "Socket.h"
+#include "SocketException.h"
 
 #ifndef DEFAULT_BUFFER_CAPACITY
 #define DEFAULT_BUFFER_CAPACITY 1400
 #endif
 
 namespace sockets {
+    namespace
+    {
+        /**
+         * Helper function that checks the state of a Connection and throws an error if it's invalid.
+         * @param function_name
+         * @param socket
+         * @param closed
+         */
+        void
+        check_connection_state(const std::string &function_name, sockets::Socket &socket, bool closed);
+
+        /**
+         * Helper functions that checks if the last bytes in the buffer are equivalent to delim
+         * @param buffer
+         * @param last_index The index of the last element in the buffer.
+         * @param delim
+         * @return
+         */
+        bool
+        check_delimiter(const Buffer<byte>& buffer, size_t last_index, const ByteString &delim);
+    }
+
     /**
      * Represents a connection between a client and a server.
      * 
      * A connection cannot be copied.
+     *
+     * An alternative implementation of Socket can be specified using the template parameter.
      */
+    template<typename T = Socket>
     class Connection
     {
     private:
-        Socket _socket;
+        T _socket;
         ByteBuffer _buffer;
         bool _closed;
     public:
-        Connection();
+        Connection() : _socket(invalid_socket), _buffer(0), _closed(true) {}
 
-        Connection(Socket socket, size_t buffer_capacity = DEFAULT_BUFFER_CAPACITY);
+        explicit Connection(T socket, size_t buffer_capacity = DEFAULT_BUFFER_CAPACITY) : _socket(socket),
+                                                                                 _buffer(buffer_capacity),
+                                                                                 _closed(false)
+        {}
 
-        ~Connection();
+        ~Connection()
+        {
+            _socket.close();
+            _closed = true;
+        }
 
         // Delete the copy constructor
         Connection(const Connection &) = delete;
@@ -47,11 +80,24 @@ namespace sockets {
          */
         template<typename Iter>
         size_t
-        read_into(Iter out);
+        read_into(Iter out)
+        { return read_into(_buffer.capacity(), out); }
 
         template<typename Iter>
         size_t
-        read_into(size_t n, Iter out);
+        read_into(size_t n, Iter out)
+        {
+            if (n > _buffer.capacity()) throw std::out_of_range("n cannot be larger than buffer.capacity()");
+            check_connection_state(__func__, _socket, _closed);
+
+            ssize_t bytes = _socket.recv(_buffer.get(), n, 0); // TODO: Add an option for flags
+            if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
+
+            //Write out the buffer
+            write_out(_buffer, static_cast<size_t>(bytes), out);
+
+            return static_cast<size_t>(bytes);
+        }
 
         /**
          * Reads up to n bytes. If no n is provided, the largest possible value will be used.
@@ -63,10 +109,20 @@ namespace sockets {
          * Returns a ByteString containing the bytes read.
          */
         ByteString
-        read_bytes();
+        read_bytes()
+        { return read_bytes(_buffer.capacity()); }
 
         ByteString
-        read_bytes(size_t n);
+        read_bytes(size_t n)
+        {
+            if (n > _buffer.capacity()) throw std::out_of_range("n cannot be larger than buffer.capacity()");
+            check_connection_state(__func__, _socket, _closed);
+
+            ssize_t bytes = _socket.recv(_buffer.get(), n, 0);
+            if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
+
+            return _buffer.to_bytestring();
+        }
 
         /**
          * Reads exactly n bytes into the given iterator. Blocks until n bytes are read.
@@ -76,13 +132,56 @@ namespace sockets {
          */
         template<typename Iter>
         void
-        read_exactly_into(size_t n, Iter out);
+        read_exactly_into(size_t n, Iter out)
+        {
+            check_connection_state(__func__, _socket, _closed);
+
+            size_t bytes_read = 0;
+
+            while (bytes_read < n)
+            {
+                size_t read_amt = std::min(_buffer.capacity(), n - bytes_read);
+
+                ssize_t bytes = _socket.recv(_buffer.get(), read_amt, 0);
+                if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
+
+                //write_out should increment the iterator as it writes, therefore we don't do it here
+                write_out(_buffer, static_cast<size_t>(bytes), out);
+                bytes_read += bytes;
+            }
+        }
 
         /**
          * Reads exactly n bytes. Blocks until n bytes are read.
          */
         ByteString
-        read_exactly_bytes(size_t n);
+        read_exactly_bytes(size_t n)
+        {
+            check_connection_state(__func__, _socket, _closed);
+
+            // Make a single allocation here to pass to bytestring
+            auto buf = ByteBuffer(n);
+            // Cursor to hold the location of the next position to read to in the buffer
+            byte *cursor = buf.begin();
+            // Count of bytes read from the socket/written to the buffer
+            size_t bytes_read = 0;
+
+            while (bytes_read < n)
+            {
+                // Calculate the number of bytes left to read to pass to recv
+                size_t read_amt = n - bytes_read;
+
+                // Read into the buffer
+                ssize_t bytes = _socket.recv(cursor, read_amt, 0);
+                if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
+
+                // Update the values
+                cursor += static_cast<size_t>(bytes);
+                bytes_read += static_cast<size_t>(bytes);
+            }
+
+            return buf.to_bytestring();
+        }
 
         /**
          * Reads bytes into the iterator until the delimiter is reached. The delimiter will also be read into the iterator.
@@ -90,28 +189,108 @@ namespace sockets {
          */
         template<typename Iter>
         size_t
-        read_until_into(const ByteString &delim, Iter out);
+        read_until_into(const ByteString &delim, Iter out)
+        {
+            if (delim.size() > _buffer.capacity()) throw std::out_of_range("delim.size() is larger than buffer capacity");
+            check_connection_state(__func__, _socket, _closed);
+
+            while (true)
+            {
+                ssize_t bytes = _socket.recv(_buffer.get(), _buffer.capacity(), 0);
+                if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
+
+                // Iterate over the buffer to read to the iterator, while checking for a delimiter
+                for (size_t i = 0; i < _buffer.capacity(); ++i)
+                {
+                    *out = _buffer[i];
+                    ++out;
+
+                    if (check_delimiter(_buffer, i, delim)) return static_cast<size_t>(bytes);
+                }
+            }
+        }
 
         /**
          * Reads until the delimiter is reached. Outputs all bytes read, including the delimiter.
          */
         ByteString
-        read_until_bytes(const ByteString &delim);
+        read_until_bytes(const ByteString &delim)
+        {
+            check_connection_state(__func__, _socket, _closed);
+
+            // Store the old capacity to restore later
+            auto old_capacity = _buffer.capacity();
+
+            // Index to keep track of where the end of the field buffer is
+            size_t bytes_read = 0;
+
+            bool stop = false;
+            while (!stop)
+            {
+                // On each iteration we create a buffer with a capacity of the remaining space in the field buffer
+                Buffer<byte> loop_buff(_buffer.capacity() - bytes_read);
+
+                ssize_t bytes = _socket.recv(loop_buff.get(), loop_buff.capacity(), 0);
+                if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
+
+                // Copy from the loop buffer to the real buffer, while checking for a delimiter
+                for(size_t i = 0; i < loop_buff.capacity(); ++i)
+                {
+                    _buffer[bytes_read] = loop_buff[i];
+                    ++bytes_read;
+
+                    if(check_delimiter(_buffer, bytes_read, delim))
+                    {
+                        stop = true;
+                        break;
+                    }
+                }
+
+                // Resize the real buffer if it's full
+                if(bytes_read == _buffer.capacity())
+                    _buffer.resize(_buffer.capacity() + bytes);
+            }
+
+            auto rv = _buffer.to_bytestring();
+            _buffer.resize(old_capacity);
+            return rv;
+        }
 
         /**
          * Writes bytes to the connection. Returns the number of bytes written.
          */
         template<typename Iter>
         size_t
-        write(Iter begin, Iter end);
+        write(Iter begin, Iter end)
+        {
+            check_connection_state(__func__, _socket, _closed);
+
+            _buffer.write(begin, end);
+
+            ssize_t bytes = _socket.send(_buffer.get(), _buffer.capacity(), 0);
+            if(bytes == -1) throw SocketError("Connection", __func__, "error on send()", get_error_code());
+
+            return static_cast<size_t>(bytes);
+        }
 
         size_t
-        write(const ByteString &data);
+        write(const ByteString &data)
+        {
+            check_connection_state(__func__, _socket, _closed);
+
+            ssize_t bytes = _socket.send(data.cbegin(), data.size(), 0);
+            if(bytes == -1) throw SocketError("Connection", __func__, "error on send()", get_error_code());
+
+            return static_cast<size_t>(bytes);
+        }
 
         /**
          * Returns true if closed. False if otherwise.
          */
         bool
-        closed();
+        closed()
+        {
+            return _closed;
+        }
     };
 }
--- src/common/Connection.cpp
+++ src/common/Connection.cpp
@@ -4,237 +4,29 @@
 #include <Connection.h>
 #include <SocketException.h>
 
-/**
- * Helper function that checks the state of a Connection and throws an error if it's invalid.
- * @param function_name
- * @param socket
- * @param closed
- */
-void
-check_connection_state(const std::string &function_name, sockets::Socket &socket, bool closed)
-{
-    if (closed) throw sockets::ClosedError("Connection", function_name);
-    if (socket.socket == sockets::invalid_socket)
-        throw sockets::SocketError("Connection", function_name, "invalid socket", EBADF);
-}
-
-/**
- * Helper functions that checks if the last bytes in the buffer are equivalent to delim
- * @param buffer
- * @param last_index The index of the last element in the buffer.
- * @param delim
- * @return
- */
-bool
-check_delimiter(const Buffer<byte>& buffer, size_t last_index, const ByteString &delim)
-{
-    if (last_index < delim.size()) return false;
-
-    size_t buffer_index = last_index - delim.size();
-    for (size_t j = 0; j < delim.size(); ++j, ++buffer_index)
-    {
-        if (buffer[buffer_index] != delim.at(j)) return false;
-    }
-    return true;
-}
 
 namespace sockets {
-
-    Connection::Connection() : _socket(invalid_socket), _buffer(0), _closed(true)
-    {}
-
-    Connection::Connection(Socket socket, size_t buffer_capacity) : _socket(socket),
-                                                                   _buffer(buffer_capacity),
-                                                                   _closed(false)
-    {}
-
-    Connection::~Connection()
+    namespace
     {
-        _socket.close();
-        _closed = true;
-    }
-
-    template<typename Iter>
-    size_t
-    Connection::read_into(Iter out)
-    { return read_into(_buffer.capacity(), out); }
-
-    template<typename Iter>
-    size_t
-    Connection::read_into(size_t n, Iter out)
-    {
-        if (n > _buffer.capacity()) throw std::out_of_range("n cannot be larger than buffer.capacity()");
-        check_connection_state(__func__, _socket, _closed);
-
-        ssize_t bytes = _socket.recv(_buffer.get(), n, 0); // TODO: Add an option for flags
-        if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
-
-        //Write out the buffer
-        write_out(_buffer, static_cast<size_t>(bytes), out);
-
-        return static_cast<size_t>(bytes);
-    }
-
-    ByteString
-    Connection::read_bytes()
-    { return read_bytes(_buffer.capacity()); }
-
-    ByteString
-    Connection::read_bytes(size_t n)
-    {
-        if (n > _buffer.capacity()) throw std::out_of_range("n cannot be larger than buffer.capacity()");
-        check_connection_state(__func__, _socket, _closed);
-
-        ssize_t bytes = _socket.recv(_buffer.get(), n, 0);
-        if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
-
-        return _buffer.to_bytestring();
-    }
-
-    template<typename Iter>
-    void
-    Connection::read_exactly_into(size_t n, Iter out)
-    {
-        check_connection_state(__func__, _socket, _closed);
-
-        size_t bytes_read = 0;
-
-        while (bytes_read < n)
+        void
+        check_connection_state(const std::string &function_name, sockets::Socket &socket, bool closed)
         {
-            size_t read_amt = std::min(_buffer.capacity(), n - bytes_read);
-
-            ssize_t bytes = _socket.recv(_buffer.get(), read_amt, 0);
-            if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
-
-            //write_out should increment the iterator as it writes, therefore we don't do it here
-            write_out(_buffer, static_cast<size_t>(bytes), out);
-            bytes_read += bytes;
-        }
-    }
-
-    ByteString
-    Connection::read_exactly_bytes(size_t n)
-    {
-        check_connection_state(__func__, _socket, _closed);
-
-        // Make a single allocation here to pass to bytestring
-        auto buf = ByteBuffer(n);
-        // Cursor to hold the location of the next position to read to in the buffer
-        byte *cursor = buf.begin();
-        // Count of bytes read from the socket/written to the buffer
-        size_t bytes_read = 0;
-
-        while (bytes_read < n)
-        {
-            // Calculate the number of bytes left to read to pass to recv
-            size_t read_amt = n - bytes_read;
-
-            // Read into the buffer
-            ssize_t bytes = _socket.recv(cursor, read_amt, 0);
-            if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
-
-            // Update the values
-            cursor += static_cast<size_t>(bytes);
-            bytes_read += static_cast<size_t>(bytes);
+            if (closed) throw sockets::ClosedError("Connection", function_name);
+            if (socket.socket == sockets::invalid_socket)
+                throw sockets::SocketError("Connection", function_name, "invalid socket", EBADF);
         }
 
-        return buf.to_bytestring();
-    }
-
-    template<typename Iter>
-    size_t
-    Connection::read_until_into(const ByteString &delim, Iter out)
-    {
-        if (delim.size() > _buffer.capacity()) throw std::out_of_range("delim.size() is larger than buffer capacity");
-        check_connection_state(__func__, _socket, _closed);
-
-        while (true)
+        bool
+        check_delimiter(const Buffer<byte>& buffer, size_t last_index, const ByteString &delim)
         {
-            ssize_t bytes = _socket.recv(_buffer.get(), _buffer.capacity(), 0);
-            if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
+            if (last_index < delim.size()) return false;
 
-            // Iterate over the buffer to read to the iterator, while checking for a delimiter
-            for (size_t i = 0; i < _buffer.capacity(); ++i)
+            size_t buffer_index = last_index - delim.size();
+            for (size_t j = 0; j < delim.size(); ++j, ++buffer_index)
             {
-                *out = _buffer[i];
-                ++out;
-
-                if (check_delimiter(_buffer, i, delim)) return static_cast<size_t>(bytes);
+                if (buffer[buffer_index] != delim.at(j)) return false;
             }
+            return true;
         }
-    }
-
-    ByteString
-    Connection::read_until_bytes(const ByteString &delim)
-    {
-        check_connection_state(__func__, _socket, _closed);
-
-        // Store the old capacity to restore later
-        auto old_capacity = _buffer.capacity();
-
-        // Index to keep track of where the end of the field buffer is
-        size_t bytes_read = 0;
-
-        bool stop = false;
-        while (!stop)
-        {
-            // On each iteration we create a buffer with a capacity of the remaining space in the field buffer
-            Buffer<byte> loop_buff(_buffer.capacity() - bytes_read);
-
-            ssize_t bytes = _socket.recv(loop_buff.get(), loop_buff.capacity(), 0);
-            if (bytes == -1) throw SocketError("Connection", __func__, "error on recv()", get_error_code());
-
-            // Copy from the loop buffer to the real buffer, while checking for a delimiter
-            for(size_t i = 0; i < loop_buff.capacity(); ++i)
-            {
-                _buffer[bytes_read] = loop_buff[i];
-                ++bytes_read;
-
-                if(check_delimiter(_buffer, bytes_read, delim))
-                {
-                    stop = true;
-                    break;
-                }
-            }
-
-            // Resize the real buffer if it's full
-            if(bytes_read == _buffer.capacity())
-                _buffer.resize(_buffer.capacity() + bytes);
-        }
-
-        auto rv = _buffer.to_bytestring();
-        _buffer.resize(old_capacity);
-        return rv;
-    }
-
-    template<typename Iter>
-    size_t
-    Connection::write(Iter begin, Iter end)
-    {
-        check_connection_state(__func__, _socket, _closed);
-
-        _buffer.write(begin, end);
-
-        ssize_t bytes = _socket.send(_buffer.get(), _buffer.capacity(), 0);
-        if(bytes == -1) throw SocketError("Connection", __func__, "error on send()", get_error_code());
-
-        return static_cast<size_t>(bytes);
-    }
-
-    size_t
-    Connection::write(const ByteString &data)
-    {
-        check_connection_state(__func__, _socket, _closed);
-
-        ssize_t bytes = _socket.send(data.cbegin(), data.size(), 0);
-        if(bytes == -1) throw SocketError("Connection", __func__, "error on send()", get_error_code());
-
-        return static_cast<size_t>(bytes);
-    }
-
-    bool
-    Connection::closed()
-    {
-        return _closed;
     }
 }
