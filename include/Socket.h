60d6d85021bcb4541b68c26b7cced2392fcb92b3
Isaac Avram <avrisaac555@gmail.com>
2018-09-11 15:02:59 -0500

    Revert "added virtual to all functions in Socket"
    This reverts commit 93db12e

--- include/Socket.h
+++ include/Socket.h
@@ -30,58 +30,58 @@ namespace sockets {
          */
         Socket(sock_addr_family, sock_type, sock_proto);
 
-        virtual Socket
+        Socket
         accept(i_addr_ptr address, i_addr_len_t* address_length);
 
-        virtual int
+        int
         bind(i_addr_cptr address, i_addr_len_t address_length);
 
-        virtual int
+        int
         connect(i_addr_cptr address, i_addr_len_t address_length);
 
-        virtual int
+        int
         getpeername(i_addr_ptr address_out, i_addr_len_t* address_length_out);
 
-        virtual int
+        int
         getsockname(i_addr_ptr address_out, i_addr_len_t* address_length_out);
 
-        virtual int
+        int
         listen(int backlog);
 
-        virtual ssize_t
+        ssize_t
         recvfrom(i_buff_t buffer, i_buff_len_t length, int flags, i_addr_ptr address, i_addr_len_t *address_len);
 
-        virtual ssize_t
+        ssize_t
         recvfrom(byte* buffer, size_t length, int flags, i_addr_ptr address, i_addr_len_t* address_len);
 
-        virtual ssize_t
+        ssize_t
         recv(i_buff_t buffer, i_buff_len_t length, int flags);
 
-        virtual ssize_t
+        ssize_t
         recv(byte* buffer, size_t length, int flags);
 
-        virtual ssize_t
+        ssize_t
         recv(std::unique_ptr<byte[]> &buffer, size_t length, int flags);
 
-        virtual ssize_t
+        ssize_t
         sendto(i_cbuff_t buffer, i_buff_len_t length, int flags, i_addr_cptr address, i_addr_len_t address_len);
 
-        virtual ssize_t
+        ssize_t
         sendto(byte* buffer, size_t length, int flags, i_addr_cptr address, i_addr_len_t address_len);
 
-        virtual ssize_t
+        ssize_t
         sendto(std::unique_ptr<byte[]> &, size_t length, int flags, i_addr_cptr address, i_addr_len_t address_len);
 
-        virtual ssize_t
+        ssize_t
         send(i_cbuff_t buffer, i_buff_len_t length, int flags);
 
-        virtual ssize_t
+        ssize_t
         send(const byte* buffer, size_t length, int flags);
 
-        virtual ssize_t
+        ssize_t
         send(const std::unique_ptr<byte[]> &, size_t length, int flags);
 
-        virtual int
+        int
         close();
     };
 }
