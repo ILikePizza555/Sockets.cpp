729a0664f59081675e23db428b44e3f30579547a
Isaac Avram <avrisaac555@gmail.com>
2018-09-19 13:36:00 -0500

    Made ByteString into std::array

--- include/Byte.h
+++ include/Byte.h
@@ -6,48 +6,5 @@
 using byte = unsigned char;
 using ByteBuffer = std::vector<byte>;
 
-/**
- * Simple class consisting of a pointer to an array of bytes and it's length.
- *
- * This class is intended for use in moving around small amounts of data, where using ByteBuffer(std::vector<byte>)
- * doesn't make sense.
- */
-class ByteString
-{
-private:
-    std::unique_ptr<byte[]> _data;
-public:
-    const size_t size;
-
-    /**
-     * Constructs an empty ByteString
-     */
-    ByteString();
-
-    /**
-     * Constructs a ByteString by copying data from a buffer.
-     * @param buffer
-     */
-    explicit ByteString(ByteBuffer buffer);
-
-    /**
-     * Constructs a ByteString from a list of bytes
-     * @param bytes
-     */
-    ByteString(std::initializer_list<byte> bytes);
-
-    /**
-     * Constructs a ByteString by taking ownership of a pointer
-     * @param size
-     * @param data
-     */
-    ByteString(size_t size, std::unique_ptr<byte[]> data);
-
-    byte* begin();
-    byte* end();
-    const byte* cbegin() const;
-    const byte* cend() const;
-
-    byte& operator[](size_t n);
-    const byte& operator[](size_t n) const;
-};
+template<size_t size>
+using ByteString = std::array<byte, size>;
