86b30f428b1b5bacf31269f9fd8238045cfdca79
Isaac Avram <avrisaac555@gmail.com>
2018-09-21 17:31:08 -0500

    check_connection_state is a templated function

--- include/Connection.h
+++ include/Connection.h
@@ -18,8 +18,14 @@ namespace sockets {
          * @param socket
          * @param closed
          */
+        template<typename T = Socket>
         void
-        check_connection_state(const std::string &function_name, sockets::Socket &socket, bool closed);
+        check_connection_state(const std::string &function_name, T &socket, bool closed)
+        {
+            if (closed) throw sockets::ClosedError("Connection", function_name);
+            if (socket.socket == sockets::invalid_socket)
+                throw sockets::SocketError("Connection", function_name, "invalid socket", EBADF);
+        }
     }
 
     /**
