a44385164d1a4f4c092deac20a459fb1fdd5d389
Isaac Avram <avrisaac555@gmail.com>
2018-09-27 21:25:58 -0500

    Cleaned up SocketException.h

--- include/SocketException.h
+++ include/SocketException.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <exception>
+#include <functional>
 #include <string>
 #include "abl/abl.h"
 
@@ -15,66 +16,46 @@ namespace sockets {
     std::string
     get_error_message(int code);
 
-    class _Exception : public std::exception
+    /**
+     * Error thrown when a C-library function returns an error.
+     */
+    class MethodError : public std::exception
     {
     public:
-        /** Throwing class name */
-        const std::string class_name;
-        /** Throwing function name */
-        const std::string function_name;
+        /** Name of the function throwing the error */
+        const std::string throwing_function_name;
+        /** Name of the function causing the error */
+        const std::string erroring_function_name;
+        /** Error code the function gave. */
+        const int error_code;
 
-        _Exception(std::string function_name);
+        using lookup_t = std::function<std::string(int)>;
+        const lookup_t lookup;
 
-        _Exception(std::string class_name, std::string function_name);
+        MethodError(std::string tfn, std::string efn, int ec, lookup_t = {});
 
-        std::string
-        full_name() const;
+        /**
+         * @return A string containing the error message.
+         */
+        virtual std::string
+        string() const;
+
+        const char*
+        what() const noexcept override;
     };
 
     /**
      * Thrown when an operation is attempted on an invalid socket
      */
-    class InvalidSocketError : public _Exception
+    class InvalidSocketError : std::exception
     {
     public:
+        const std::string class_name;
+        const std::string function_name;
+
         InvalidSocketError(std::string class_name, std::string function_name);
 
-        virtual const char *
-        what() const noexcept;
-    };
-
-    class ClosedError : public _Exception
-    {
-    public:
-        ClosedError(std::string class_name, std::string function);
-
-        virtual const char *
-        what() const noexcept;
-    };
-
-    class SocketError : public _Exception
-    {
-    public:
-        const int error_code;
-        const std::string message;
-
-        SocketError(std::string class_name, std::string function_name, std::string message, int error_code);
-
-        SocketError(std::string function_name, std::string message, int error_code);
-
-        virtual const char *
-        what() const noexcept;
-    };
-
-    class AddressInfoError : public std::exception
-    {
-    public:
-        std::string function_name;
-        const int error_value;
-
-        AddressInfoError(std::string function_name, int error_value);
-
-        virtual const char *
-        what() const noexcept;
+        const char *
+        what() const noexcept override;
     };
 }
--- src/common/SocketException.cpp
+++ src/common/SocketException.cpp
@@ -1,47 +1,39 @@
+#include <utility>
+
 #include <SocketException.h>
 #include <sstream>
 
 namespace sockets {
-    _Exception::_Exception(std::string function_name) : class_name(""), function_name(function_name) {}
-    _Exception::_Exception(std::string class_name, std::string function_name) : class_name(class_name), function_name(function_name) {}
-    std::string _Exception::full_name() const
+    MethodError::MethodError(std::string tfn, std::string efn, int ec, MethodError::lookup_t lookup) :
+    throwing_function_name(std::move(tfn)), erroring_function_name(std::move(efn)), error_code(ec), lookup(lookup) {}
+
+    std::string MethodError::string() const
     {
-        if(class_name.empty()) return class_name + "::" + function_name;
-        return function_name;
+        std::stringstream ss;
+        ss << "MethodError error thrown by " << this->throwing_function_name << " ";
+        ss << "when calling " << erroring_function_name << ". ";
+        ss << "Error code: " << error_code;
+
+        if(lookup)
+            ss << ": " << lookup(error_code);
+
+        return ss.str();
     }
 
-    InvalidSocketError::InvalidSocketError(std::string class_name, std::string function_name) : _Exception(class_name, function_name) {}
+    const char*
+    MethodError::what() const noexcept
+    {
+        return string().c_str();
+    }
+
+    InvalidSocketError::InvalidSocketError(std::string class_name, std::string function_name) :
+    class_name(std::move(class_name)), function_name(std::move(function_name))
+    {}
+
     const char* InvalidSocketError::what() const noexcept
     {
         std::stringstream ss;
-        ss << _Exception::full_name() << ": invalid socket";
-        return ss.str().c_str();
-    }
-
-    ClosedError::ClosedError(std::string class_name, std::string function_name) : _Exception(class_name, function_name) {}
-    const char* ClosedError::what() const noexcept
-    {
-        std::stringstream ss;
-        ss << full_name() << ": " << class_name << " is closed";
-        return ss.str().c_str();    
-    }
-
-    SocketError::SocketError(std::string class_name, std::string function_name, std::string message, int error_code) : _Exception(class_name, function_name), error_code(error_code), message(message) {}
-    SocketError::SocketError(std::string function_name, std::string message, int error_code) : _Exception(function_name), error_code(error_code), message(message) {}
-    const char* SocketError::what() const noexcept
-    {
-        std::stringstream ss;
-        ss << full_name() << ": " << message << ": " << error_code << ": " << get_error_message(error_code);
-        return ss.str().c_str();
-    }
-
-    AddressInfoError::AddressInfoError(std::string function_name, int error_value) : function_name(function_name), error_value(error_value)
-    {}
-    const char* AddressInfoError::what() const noexcept
-    {
-        std::stringstream ss;
-        // TODO: gai_strerror() and WSAGetLastError on Windows
-        ss << function_name << ": getaddrinfo: " << error_value;
+        ss << class_name << "::" << function_name << ": invalid socket";
         return ss.str().c_str();
     }
 }
