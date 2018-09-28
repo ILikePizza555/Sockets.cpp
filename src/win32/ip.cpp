e6fafb4ff7d46b350898de06bee1a9b499ab6439
Isaac Avram <avrisaac555@gmail.com>
2018-09-27 22:36:37 -0500

    Implemented is_loopback

--- src/win32/ip.cpp
+++ src/win32/ip.cpp
@@ -4,10 +4,29 @@
 
 #include <abl/ip.h>
 #include <Error.h>
+#include <inaddr.h>
+#include <ws2ipdef.h>
+#include <in6addr.h>
 
-bool sockets::addr_t::is_local()
+bool sockets::addr_t::is_loopback()
 {
+    if(this->addr_ptr->sa_family == ip_family::INET)
+    {
+        auto* a = reinterpret_cast<const sockaddr_in*>(this->addr_ptr.get());
+        return (a->sin_addr.S_un.S_addr & 0xFF000000) == 0x7F000000;
+    }
 
+    if(this->addr_ptr->sa_family == ip_family::INET6)
+    {
+        auto* a = reinterpret_cast<const sockaddr_in6*>(this->addr_ptr.get());
+        auto& ip_bytes = a->sin6_addr.u.Word;
+
+        return ip_bytes[0] == 0 && ip_bytes[1] == 0 && ip_bytes[2] == 0 &&
+               ip_bytes[3] == 0 && ip_bytes[4] == 0 && ip_bytes[5] == 0 &&
+               ip_bytes[6] == 0 && ip_bytes[7] == 1;
+    }
+
+    return false;
 }
 
 std::string sockets::addr_t::name()
