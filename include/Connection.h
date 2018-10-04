fd4a2373f9e0ce17ddef847fc2883c560e4286bf
Isaac Avram <avrisaac555@gmail.com>
2018-10-03 17:05:57 -0700

    write(Iter, Iter) now actually writes the data

--- include/Connection.h
+++ include/Connection.h
@@ -181,6 +181,13 @@ namespace sockets {
         {
             check_connection_state(__func__, _socket, _closed);
 
+            auto distance = std::distance(begin, end);
+            if(distance < 0) throw std::invalid_argument("begin and end have a negative distance");
+            if(distance == 0) return 0;
+
+            _buffer.reserve(static_cast<size_t>(distance));
+            std::copy(begin, end, _buffer.begin());
+
             ssize_t bytes = _socket.send(_buffer, _buffer.capacity(), 0);
             if(bytes == -1) throw MethodError(__func__, "send", get_error_code(), get_error_message);
 
