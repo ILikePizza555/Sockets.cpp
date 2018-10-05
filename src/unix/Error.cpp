730cb5303684144634cb318fa0505143d40a31fe
Isaac Avram <avrisaac555@gmail.com>
2018-10-04 17:14:00 -0700

    Implemented unix part

--- src/unix/Error.cpp
+++ src/unix/Error.cpp
@@ -17,4 +17,48 @@ namespace sockets {
     {
         return errno == ECONNRESET;
     }
+
+    SocketReadError::ErrorType SocketReadError::map_error_type(int code)
+    {
+        switch (code)
+        {
+            case EWOULDBLOCK:
+                return ErrorType::WOULD_BLOCK;
+            case EBADF:
+                return ErrorType::INVALID_HANDLE;
+            case ENOTSOCK:
+                return ErrorType::NOT_SOCKET;
+            case ETIMEDOUT:
+                return ErrorType::TIMED_OUT;
+            case EINTR:
+                return ErrorType::INTERRUPTED;
+            case ENOTCONN:
+                return ErrorType::NOT_CONNECTED;
+            default:
+                return ErrorType::OTHER;
+        }
+    }
+
+    SocketWriteError::ErrorType SocketWriteError::map_error_type(int code)
+    {
+        switch (code)
+        {
+            case ETIMEDOUT:
+                return ErrorType::TIMED_OUT;
+            case EWOULDBLOCK:
+                return ErrorType::WOULD_BLOCK;
+            case EBADF:
+                return ErrorType::INVALID_HANDLE;
+            case ENOTSOCK:
+                return ErrorType::NOT_SOCKET;
+            case ENOTCONN:
+                return ErrorType::NOT_CONNECTED;
+            case EINTR:
+                return ErrorType::INTERRUPTED;
+            case ECONNRESET:
+                return ErrorType::CONNECTION_RESET;
+            default:
+                return ErrorType::OTHER;
+        }
+    }
 }
