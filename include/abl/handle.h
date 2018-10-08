75181e6b18ec58aa6ab4a91e5ff13233e540edf5
Isaac Avram <avrisaac555@gmail.com>
2018-10-07 17:25:14 -0700

    Removed functions

--- include/abl/handle.h
+++ include/abl/handle.h
@@ -13,39 +13,5 @@ namespace sockets {
          * Struct that contains the system socket handle.
          */
         struct handle_t;
-
-        namespace {
-            /**
-             * Type unsafe method for getting the system handle type.
-             * @param handle
-             * @return
-             */
-            void* get_system_handle(const handle_t* handle);
-            void* get_system_handle(const std::unique_ptr<handle_t>& handle);
-            void* get_system_handle(const std::shared_ptr<handle_t>& handle);
-        }
-
-        /**
-         * Returns the system handle stored in handle_t
-         * @tparam SystemType The type of the handle the system uses.
-         * @return
-         */
-        template<typename SystemType>
-        SystemType get_handle(const handle_t* handle)
-        {
-            return *reinterpret_cast<SystemType*>(get_system_handle(handle));
-        }
-
-        template<typename SystemType>
-        SystemType get_handle(const std::unique_ptr<handle_t>& handle)
-        {
-            return *reinterpret_cast<SystemType>(get_system_handle(handle));
-        }
-
-        template<typename SystemType>
-        SystemType get_handle(const std::shared_ptr<handle_t>& handle)
-        {
-            return *reinterpret_cast<SystemType>(get_system_handle(handle));
-        }
     }
 }
