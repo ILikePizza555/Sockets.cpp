399a79d0d665a1b4c4510a84ad76ae7a5f4b8b8d
Isaac Avram <avrisaac555@gmail.com>
2018-10-11 20:51:47 -0700

    Added error-checking

--- src/win32/handle.cpp
+++ src/win32/handle.cpp
@@ -2,6 +2,7 @@
 // Created by avris on 10/4/2018.
 //
 
+#include <Error.h>
 #include <abl/handle.h>
 #include <abl/system.h>
 
@@ -40,12 +41,20 @@ namespace sockets {
         UniqueHandle new_unique_handle(ip_family family, sock_type type, sock_proto protocol)
         {
             SOCKET s = socket(iftosys(family), sttosys(type), sptosys(protocol));
+
+            if(s == INVALID_SOCKET)
+                throw MethodError(__func__, "socket");
+
             return UniqueHandle(new handle_t{s}, &close_handle);
         }
 
         SharedHandle new_shared_handle(ip_family family, sock_type type, sock_proto protocol)
         {
             SOCKET s = socket(iftosys(family), sttosys(type), sptosys(protocol));
+
+            if(s == INVALID_SOCKET)
+                throw MethodError(__func__, "socket");
+
             return SharedHandle(new handle_t{s}, &close_handle);
         }
     }
