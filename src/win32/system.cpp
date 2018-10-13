7bee04bdad89867c477689f0c3a94a296bf87420
Isaac Avram <avrisaac555@gmail.com>
2018-10-12 22:58:00 -0700

    Implemented some conversion functions

--- src/win32/system.cpp
+++ src/win32/system.cpp
@@ -21,6 +21,18 @@ namespace sockets {
             return -1;
         }
 
+        ip_family system::systoif(int family)
+        {
+            switch (family) {
+                case AF_INET:
+                    return ip_family::INET;
+                case AF_INET6:
+                    return ip_family::INET6;
+                default:
+                    return ip_family::ANY;
+            }
+        }
+
         int system::sttosys(sockets::abl::sock_type type)
         {
             switch (type) {
@@ -34,6 +46,18 @@ namespace sockets {
             return -1;
         }
 
+        sock_type system::systost(int type)
+        {
+            switch (type) {
+                case SOCK_STREAM:
+                    return sock_type::STREAM;
+                case SOCK_DGRAM:
+                    return sock_type::DATAGRAM;
+                default:
+                    return sock_type::RAW;
+            }
+        }
+
         int system::sptosys(sockets::abl::sock_proto proto)
         {
             switch (proto) {
@@ -43,6 +67,18 @@ namespace sockets {
                     return IPPROTO_UDP;
             }
             return -1;
+        }
+
+        sock_proto system::systosp(int proto)
+        {
+            switch (proto) {
+                case IPPROTO_TCP:
+                    return sock_proto::TCP;
+                case IPPROTO_UDP:
+                    return sock_proto::UDP;
+                default:
+                    throw std::invalid_argument("proto");
+            }
         }
 
         sockaddr_in system::ipv4_to_system(const sockets::abl::ipv4_addr &address)
