c19f44438ca83d9cac447f7f9be2f1e252034e0b
Isaac Avram <avrisaac555@gmail.com>
2018-10-22 12:57:42 -0700

    Implemented things in win32.cpp

--- include/abl/win32.h
+++ include/abl/win32.h
@@ -5,18 +5,30 @@
 #pragma once
 
 #include <memory>
+#include <exception>
 
 namespace sockets {
     namespace abl {
         namespace win32 {
+            class WSAError : public std::exception
+            {
+            public:
+                const int error_code;
+                const std::string function;
+
+                WSAError(int error_code, std::string function);
+
+                const char* what() const noexcept override;
+            };
+
 
             /**
              * Loads the Winsock DLL on construction, and unloads it on deconstruction.
              */
             class WinSockDLL {
             private:
-                struct WSAData;
-                std::unique_ptr<WSAData> _data;
+                struct Imp;
+                std::unique_ptr<Imp> _imp;
             public:
                 explicit WinSockDLL(unsigned char hi = 2, unsigned char low = 2);
                 ~WinSockDLL();
---
+++ src/win32/win32.cpp
@@ -0,0 +1,39 @@
+//
+// Created by avris on 10/22/2018.
+//
+
+#include <abl/win32.h>
+#include <winsock2.h>
+#include <sstream>
+
+namespace sockets {
+    abl::win32::WSAError::WSAError(int ec, std::string f) : error_code(ec), function(std::move(f))
+    {
+    }
+
+    const char* abl::win32::WSAError::what() const noexcept
+    {
+        std::stringstream ss;
+        ss << function << " failed with error code: " << error_code;
+        return ss.str().c_str();
+    }
+
+    struct abl::win32::WinSockDLL::Imp {
+        WSAData wsa_data{};
+    };
+
+    abl::win32::WinSockDLL::WinSockDLL(unsigned char hi, unsigned char low) : _imp(new Imp{})
+    {
+        WORD version_requested = MAKEWORD(low, hi);
+
+        int error = WSAStartup(version_requested, &_imp->wsa_data);
+        if(error != 0)
+            throw WSAError(error, "WSAStartup");
+    }
+
+    abl::win32::WinSockDLL::~WinSockDLL()
+    {
+        int error_code = WSACleanup();
+        if(error_code != 0) throw WSAError(error_code, "WSACleanup");
+    }
+}
