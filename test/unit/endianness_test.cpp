231b6380c82db5d4f45af82e66a3f54f50c3b5ce
Isaac Avram <avrisaac555@gmail.com>
2018-10-25 11:54:36 -0700

    Added endianess tests for ports

--- test/unit/endianness_test.cpp
+++ test/unit/endianness_test.cpp
@@ -76,44 +76,52 @@ sockaddr_in6 ipv6_from_array(const std::array<unsigned char, 16>& addr)
 
 TEST_CASE("from_ipv4_str creates a sockaddr_in structure in network byte order", "[Endianness][system][ipv4]")
 {
-    sockaddr_in addr = from_ipv4_str(test_ipv4_str, 0);
+    sockaddr_in addr = from_ipv4_str(test_ipv4_str, test_port);
 
     unsigned long actual = get_ipv4_as_long(addr);
 
     // Network byte order means that the most significant byte has the lowest address
     REQUIRE(actual == test_ipv4_l);
+    REQUIRE(addr.sin_port == htons(test_port));
 }
 
 TEST_CASE("from_ipv6_str creates a sockaddr_in6 structure in network byte order", "[Endianness][system][ipv6]")
 {
-    auto addr = from_ipv6_str(test_ipv6_str, 0);
+    auto addr = from_ipv6_str(test_ipv6_str, test_port);
 
     auto actual = get_ipv6_as_array(addr);
 
     REQUIRE(actual == test_ipv6_arr);
+    REQUIRE(addr.sin6_port == htons(test_port));
 }
 
 TEST_CASE("IpAddress string constructor creates an ipv4 address in network byte order", "[Endianness][IpAddress][ipv4]")
 {
-    sockets::abl::IpAddress addr(sockets::abl::ip_family::INET, test_ipv4_str, 0);
+    sockets::abl::IpAddress addr(sockets::abl::ip_family::INET, test_ipv4_str, test_port);
 
     auto actual = addr.get_as_ipv4();
 
     REQUIRE(actual.address == test_ipv4_arr);
+    REQUIRE(actual.port == htons(test_port));
+
+    REQUIRE(addr.port() == test_port);
 }
 
 TEST_CASE("IpAddress string constructor creates an ipv6 address in network byte order", "[Endianness][IpAddress][ipv6]")
 {
-    sockets::abl::IpAddress addr(sockets::abl::ip_family::INET6, test_ipv6_str, 0);
+    sockets::abl::IpAddress addr(sockets::abl::ip_family::INET6, test_ipv6_str, test_port);
 
     auto actual = addr.get_as_ipv6();
 
     REQUIRE(actual.address == test_ipv6_arr);
+    REQUIRE(actual.port == htons(test_port));
+
+    REQUIRE(addr.port() == test_port);
 }
 
 TEST_CASE("from_ipv4 creates a sockaddr_in structure in network byte order", "[Endianness][system][ipv4]")
 {
-    sockets::abl::ipv4_addr addr{test_port, test_ipv4_arr};
+    sockets::abl::ipv4_addr addr{htons(test_port), test_ipv4_arr};
 
     auto actual = from_ipv4(addr);
     unsigned long actual_l = get_ipv4_as_long(actual);
@@ -124,7 +132,7 @@ TEST_CASE("from_ipv4 creates a sockaddr_in structure in network byte order", "[E
 
 TEST_CASE("from_ipv6 creates a sockaddr_in6 structure in network byte order", "[Endianness][system][ipv6]")
 {
-    sockets::abl::ipv6_addr addr{test_port, 0, test_ipv6_arr, 0};
+    sockets::abl::ipv6_addr addr{htons(test_port), 0, test_ipv6_arr, 0};
 
     auto actual = from_ipv6(addr);
     auto actual_a = get_ipv6_as_array(actual);
@@ -135,22 +143,22 @@ TEST_CASE("from_ipv6 creates a sockaddr_in6 structure in network byte order", "[
 
 TEST_CASE("to_ipv4 creates an ipv4_addr object with an address in network byte order", "[Endianness][system][ipv4]")
 {
-    auto addr = ipv4_from_long(test_ipv4_l);
+    sockaddr_in addr = ipv4_from_long(test_ipv4_l);
     addr.sin_port = htons(test_port);
 
     auto actual = to_ipv4(addr);
 
     REQUIRE(actual.address == test_ipv4_arr);
-    REQUIRE(actual.port == test_port);
+    REQUIRE(actual.port == htons(test_port));
 }
 
 TEST_CASE("to_ipv6 creates an ipv6_addr object with an address in network byte order", "[Endianness][system][ipv6]")
 {
-    auto addr = ipv6_from_array(test_ipv6_arr);
+    sockaddr_in6 addr = ipv6_from_array(test_ipv6_arr);
     addr.sin6_port = htons(test_port);
 
     auto actual = to_ipv6(addr);
 
     REQUIRE(actual.address == test_ipv6_arr);
-    REQUIRE(actual.port == test_port);
+    REQUIRE(actual.port == htons(test_port));
 }
