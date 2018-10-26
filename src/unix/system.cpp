e9a0af77889a92043f6420c436ddd2bb9297ab82
Isaac Avram <avrisaac555@gmail.com>
2018-10-26 01:04:13 -0700

    Implemented more system functions for unix

--- src/unix/system.cpp
+++ src/unix/system.cpp
@@ -77,5 +77,32 @@ namespace sockets {
                     throw std::invalid_argument("proto");
             }
         }
+
+        sockaddr_in system::from_ipv4(const ipv4_addr &address)
+        {
+            sockaddr_in rv{AF_INET, address.port, {}, {}};
+            rv.sin_addr.s_addr = address.address[0] |
+                                 (address.address[1] << 8) |
+                                 (address.address[2] << 16) |
+                                 (address.address[3] << 24);
+            return rv;
+        }
+
+        sockaddr_in6 system::from_ipv6(const ipv6_addr &address)
+        {
+            sockaddr_in6 rv{AF_INET6, address.port, address.flowinfo, {}, address.scope_id};
+            std::copy(address.address.cbegin(), address.address.cend(), rv.sin6_addr.s6_addr);
+            return rv;
+        }
+
+        ipv4_addr system::to_ipv4(const sockaddr_in &addr)
+        {
+            ipv4_addr rv{addr.sin_port, {}};
+            rv.address[0] = static_cast<unsigned char>(addr.sin_addr.s_addr & 0xFF);
+            rv.address[1] = static_cast<unsigned char>(addr.sin_addr.s_addr >> 8 & 0xFF);
+            rv.address[2] = static_cast<unsigned char>(addr.sin_addr.s_addr >> 16 & 0xFF);
+            rv.address[3] = static_cast<unsigned char>(addr.sin_addr.s_addr >> 24 & 0xFF);
+            return rv;
+        }
     }
 }
