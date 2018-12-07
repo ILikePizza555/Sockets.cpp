21e7a501c3e7f298a5cc933d4230d8c3b782ef80
Isaac Avram <avrisaac555@gmail.com>
2018-12-06 17:49:40 -0800

    Test works

--- test/system/server_test.cpp
+++ test/system/server_test.cpp
@@ -1,5 +1,7 @@
 #include <sockets/TCPServerSocket.h>
 #include <sockets/Error.h>
+#include <sockets/Byte.h>
+#include <sockets/abl/win32.h>
 #include <tuple>
 #include <iostream>
 
@@ -11,8 +13,13 @@ using std::endl;
 
 int main(int argc, char** argv)
 {
-    TCPServerSocket server("127.0.0.1", "22");
-    cout << "Listening on port 22..." << endl;
+#ifdef _WIN32
+    sockets::abl::win32::WinSockDLL dll;
+    std::cout << "Initalized WinSock dll." << std::endl;
+#endif
+
+    TCPServerSocket server("127.0.0.1", "1022");
+    cout << "Listening on port 1022..." << endl;
 
     TCPConnection conn;
     sockets::abl::IpAddress ip;
@@ -24,7 +31,7 @@ int main(int argc, char** argv)
     {
         try {
             // Check connection state
-            conn.write<1>({1});
+            conn.write<1>(ByteString<1>{{1}});
 
             auto b = conn.read(128);
             cout << "[" << ip.name() << ":" << ip.port() << "] " << b << endl;
