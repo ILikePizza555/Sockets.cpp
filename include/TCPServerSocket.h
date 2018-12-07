184a0bfe309325d7eb9974afd530d205e3ce5b0f
Isaac Avram <avrisaac555@gmail.com>
2018-12-06 16:07:28 -0800

    Added TCPServerSocket

---
+++ include/TCPServerSocket.h
@@ -0,0 +1,30 @@
+//
+// Created by avris on 12/6/2018.
+//
+#pragma once
+
+#include "TCPSocket.h"
+#include "Connection.h"
+#include "abl/ip.h"
+
+namespace sockets {
+
+    /**
+     * Wrapper around a TCPSocket that binds to a local port and listens for incoming connections.
+     */
+    class TCPServerSocket
+    {
+    protected:
+        TCPSocket _serverSocket;
+    public:
+        /**
+         * Creates a new TCPServerSocket bound to the specified ip address
+         * @param addr
+         */
+        explicit TCPServerSocket(const abl::IpAddress& addr, int backlog = 1024);
+        TCPServerSocket(const std::string& ip, unsigned short port);
+
+        TCPConnection accept() const;
+        std::tuple<TCPConnection, abl::IpAddress> acceptfrom() const;
+    };
+}
